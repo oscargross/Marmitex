8ffbd85ba840d3681ef93175aa345d9b6d00f2ee
OSCARGROSS <OSCAR.GROSS222@GMAIL.COM>
2019-09-10 21:42:03 -0300

    oi

--- Cliente.java
+++ Cliente.java
@@ -2,33 +2,32 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Cliente {
-    Endereco endereco = new Endereco();
-    
-    private ArrayList<String> historicoPedidos = new ArrayList<>();
-    private ArrayList<String> listaClientes = new ArrayList<>();   
+    DadosCliente dados = new DadosCliente();
 
-	public boolean verificaLista(String nome_1) {
-        for (String i : this.listaClientes){
-            if (nome_1.equals(i)){
+    private ArrayList<String> historicoPedidos = new ArrayList<>();
+    private ArrayList<String> listaClientes = new ArrayList<>();
+
+    public boolean verificaLista(String nome_1) {
+        for (String i : this.listaClientes) {
+            if (nome_1.equals(i)) {
                 return true;
             }
         }
         return false;
-	}
-/*
-	public void cadastrarCliente(String nome_1) {
-        Scanner scan = new Scanner(System.in);
-        listaClientes.add(nome_1);
-        setNome(nome_1);
+    }
+    public ArrayList<String> getHistoricoPedidos() {
+        return historicoPedidos;
+    }
 
-        System.out.println("Digite seu CPF: ");
-        Integer cpf_1 = scan.nextInt();
-        setCpf(cpf_1);
+    public void setHistoricoPedidos(ArrayList<String> historicoPedidos) {
+        this.historicoPedidos = historicoPedidos;
+    }
+    public ArrayList<String> getListaClientes() {
+        return listaClientes;
+    }
 
-        System.out.println("Digite seu telefone: ");
-        Integer fone_1 = scan.nextInt();
-        setFone(fone_1);
-
-    }*/
+    public void setListaClientes(ArrayList<String> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
 
 }
