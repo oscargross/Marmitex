4627d2d92b7fc61413efb0d175ae47c5b622bda0
OSCARGROSS <OSCAR.GROSS222@GMAIL.COM>
2019-09-17 19:18:39 -0300

    oi

--- Comida.java
+++ Comida.java
@@ -1,87 +1,74 @@
 import java.util.ArrayList;
 
 class Comida {
-    ArrayList<Principais> listaPrincipais;
-    ArrayList<Saladas> listaSaladas;
-    ArrayList<Sobremesas> listaSobremesas;
-    ArrayList<Adicionais> listaAdicionais;
-    ArrayList<Bebidas> listaBebidas;
+    ArrayList<ArrayList<Componentes>> listaTodasComidas;
+    ArrayList<Componentes> listaPrincipais;
+    ArrayList<Componentes> listaSaladas;
+    ArrayList<Componentes> listaSobremesas;
+    ArrayList<Componentes> listaAdicionais;
+    ArrayList<Componentes> listaBebidas;
+
     public Comida(){
-        listaPrincipais = new ArrayList<>();
-        listaSaladas = new ArrayList<>();
-        listaSobremesas = new ArrayList<>();
-        listaAdicionais = new ArrayList<>();
-        listaBebidas = new ArrayList<>();
+        listaTodasComidas = new ArrayList<>();
+        listaPrincipais = new ArrayList<Componentes>();
+        listaSaladas = new ArrayList<Componentes>();
+        listaSobremesas = new ArrayList<Componentes>();
+        listaAdicionais = new ArrayList<Componentes>();
+        listaBebidas = new ArrayList<Componentes>();
         listaPrincipais.add(null);
         listaSaladas.add(null);
         listaSobremesas.add(null);
         listaAdicionais.add(null);
         listaBebidas.add(null);
-    }   
-    public void addPrincipais(String alimento, Double valor){
-        Principais principal = new Principais();
-        principal.setP1(alimento);
-        principal.setV1(valor);
-        listaPrincipais.add(principal);
-    }
-    public void addSaladas(String alimento, Double valor){
-        Saladas salada = new Saladas();
-        salada.setS1(alimento);
-        salada.setV1(valor);
-        listaSaladas.add(salada);
-    }
-    public void addSobremesas(String alimento, Double valor){
-        Sobremesas sobremesa = new Sobremesas();
-        sobremesa.setSo1(alimento);
-        sobremesa.setV1(valor);
-        listaSobremesas.add(sobremesa);
-    }
-    public void addAdicionais(String alimento, Double valor){
-        Adicionais adicional = new Adicionais();
-        adicional.setA1(alimento);
-        adicional.setV1(valor);
-        listaAdicionais.add(adicional);
-    }
-    public void addBebidas(String alimento, Double valor){
-        Bebidas bebida = new Bebidas();
-        bebida.setB1(alimento);
-        bebida.setV1(valor);
-        listaBebidas.add(bebida);
-    }
 
-    public void mostrarPrincipais() {
-        for (int i = 1; i < listaPrincipais.size(); i++) {
-            System.out.print(i);
-            System.out.println(listaPrincipais.get(i));
+        listaTodasComidas.add(null);
+        listaTodasComidas.add(listaSaladas);
+        listaTodasComidas.add(listaPrincipais);
+        listaTodasComidas.add(listaAdicionais);
+        listaTodasComidas.add(listaSobremesas);
+        listaTodasComidas.add(listaBebidas);
+
+    }   
+    public ArrayList<Componentes> addComida(String alimento, Double valor, ArrayList<Componentes> listaTempComidas) {
+        Componentes tipoComida = new Componentes();
+        tipoComida.setAlimento(alimento);
+        tipoComida.setValor(valor);
+        listaTempComidas.add(tipoComida);
+        return listaTempComidas;
+    }
+    public void mostrarListaComidas(ArrayList<Componentes> listaComidasMostrar) {
+        listaComidasMostrar = new ArrayList<Componentes>();
+
+        for (int i = 1; i < listaComidasMostrar.size(); i++) {
+            System.out.print(i+" - ");
+            System.out.println(listaComidasMostrar.get(i));
         }
         System.out.println("\n");
     }
-    public void mostrarSaladas() {
-        for (int i = 1; i < listaSaladas.size(); i++) {
-            System.out.print(i);
-            System.out.println(listaSaladas.get(i));
+    
+    public void toString(ArrayList<Componentes> listaComidas, int qualLista) {
+        listaComidas = new ArrayList<Componentes>();
+        if(qualLista==1){  
+            System.out.println("SALADAS\n");
+        }else if(qualLista==2){
+            System.out.println("PRICIPAIS\n");
+        }else if(qualLista==3){
+            System.out.println("ADICIONAIS\n");
+        }else if(qualLista==2){
+            System.out.println("SOBREMESAS\n");
+        }else if(qualLista==2){
+            System.out.println("BEBIDAS\n");
         }
-        System.out.println("\n");
-    }  
-    public void mostrarSobremesas() {
-        for (int i = 1; i < listaSobremesas.size(); i++) {
-            System.out.print(i);
-            System.out.println(listaSobremesas.get(i));
+        System.out.println("Escolha um dos itens:\n");
+        System.out.println(listaComidas);
+
+        for (int i = 1; i < (listaComidas.size()); i++) {
+            System.out.print(i+" - ");
+            System.out.println(listaComidas.get(i));
         }
-        System.out.println("\n");
-    }    
-    public void mostrarAdicionais() {
-        for (int i = 1; i < listaAdicionais.size(); i++) {
-            System.out.print(i);
-            System.out.println(listaAdicionais.get(i));
-        }
-        System.out.println("\n");
+        System.out.println("\n");         
     }
-    public void mostrarBebidas() {
-        for (int i = 1; i < listaBebidas.size(); i++) {
-            System.out.print(i);
-            System.out.println(listaBebidas.get(i));
-        }
-        System.out.println("\n"); 
-    }  
+  
+
+
 }
--- Marmita.java
+++ Marmita.java
@@ -2,67 +2,71 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Marmita {
-    Scanner scan = new Scanner(System.in);//seção critica é a parte do codigo que pode ser acessada opr 2 processos ao mesmo tempo
 
     public static void main(String[] args) {
         Comida refeicao = new Comida();
+        ArrayList<Componentes> saladas = refeicao.listaSaladas;
+        ArrayList<Componentes> principais = refeicao.listaPrincipais;
+        ArrayList<Componentes> adicionais = refeicao.listaAdicionais;
+        ArrayList<Componentes> sobremesas = refeicao.listaSobremesas;
+        ArrayList<Componentes> bebidas = refeicao.listaBebidas;
+        saladas = new ArrayList<Componentes>();
+        principais = new ArrayList<Componentes>();
+        adicionais = new ArrayList<Componentes>();
+        sobremesas = new ArrayList<Componentes>();
+        bebidas = new ArrayList<Componentes>();
 
         //Adicionar Saladas ao menu.
-        refeicao.addSaladas("Alface", 1.00);
-        refeicao.addSaladas("Tomate", 1.20);
-        refeicao.addSaladas("Couve-Flor", 1.40);
-        refeicao.addSaladas("rucula", 1.20);
-        refeicao.addSaladas("Cenoura e Beterraba", 1.30);
-        refeicao.addSaladas("Cenoura e Beterraba", 1.30);
+        refeicao.addComida("Alface", 1.00, saladas);
+        refeicao.addComida("Tomate", 1.20, saladas);
+        refeicao.addComida("Couve-Flor", 1.40, saladas);
+        refeicao.addComida("rucula", 1.20, saladas);
+        refeicao.addComida("Cenoura e Beterraba", 1.30, saladas);
+        refeicao.addComida("Cenoura e Beterraba", 1.30, saladas);
 
-        //Adicionar Partos Principais ao menu
-        refeicao.addPrincipais("Arroz, Feijão e batata frita", 5.50);
-        refeicao.addPrincipais("Massa com molho de galinha", 6.50);
-        refeicao.addPrincipais("Panquecas", 7.00);
-        refeicao.addPrincipais("Arroz com galinha", 4.50);
-        refeicao.addPrincipais("Lasanha 4 queijos", 7.00);
-        refeicao.addPrincipais("Purê e guizado", 6.50);
-        refeicao.addPrincipais("Churrasco e maioneses", 7.50);
+        
+        //Adicionar Pratos Principais ao menu
+        refeicao.addComida("Arroz, Feijão e batata frita", 5.50, principais);
+        refeicao.addComida("Massa com molho de galinha", 6.50, principais);
+        refeicao.addComida("Panquecas", 7.00, principais);
+        refeicao.addComida("Arroz com galinha", 4.50, principais);
+        refeicao.addComida("Lasanha 4 queijos", 7.00, principais);
+        refeicao.addComida("Purê e guizado", 6.50, principais);
+        refeicao.addComida("Churrasco e maioneses", 7.50, principais);
 
         //Adicionar Adicionais ao menu
-        refeicao.addAdicionais("Carne de porco", 1.7);
-        refeicao.addAdicionais("Carne de gado", 1.8);
-        refeicao.addAdicionais("Carne de frango", 1.6);
-        refeicao.addAdicionais("Queijo - feito na chapa", 1.3);
+        refeicao.addComida("Carne de porco", 1.7, adicionais);
+        refeicao.addComida("Carne de gado", 1.8, adicionais);
+        refeicao.addComida("Carne de frango", 1.6, adicionais);
+        refeicao.addComida("Queijo - feito na chapa", 1.3, adicionais);
          
         //Adicionar Sobremesas ao menu
-        refeicao.addSobremesas("sagu", 0.9);
-        refeicao.addSobremesas("Torta de bolacha", 2.00);
-        refeicao.addSobremesas("Ambrosia", 1.0);
-        refeicao.addSobremesas("Pudim", 1.80);
-        refeicao.addSobremesas("Mousse de maracujá", 1.5);
+        refeicao.addComida("sagu", 0.9, sobremesas);
+        refeicao.addComida("Torta de bolacha", 2.00, sobremesas);
+        refeicao.addComida("Ambrosia", 1.0, sobremesas);
+        refeicao.addComida("Pudim", 1.80, sobremesas);
+        refeicao.addComida("Mousse de maracujá", 1.5, sobremesas);
         
         //Adicionar Bebidas ao menu
-        refeicao.addBebidas("Coca Cola", 5.0);
-        refeicao.addBebidas("Pepsi", 4.5); 
-        refeicao.addBebidas("Suco de Laranja", 4.00);
-        refeicao.addBebidas("Suco de Uva", 4.00);
-        refeicao.addBebidas("Agua", 2.0);
+        refeicao.addComida("Coca Cola", 5.0, bebidas);
+        refeicao.addComida("Pepsi", 4.5, bebidas); 
+        refeicao.addComida("Suco de Laranja", 4.00, bebidas);
+        refeicao.addComida("Suco de Uva", 4.00, bebidas);
+        refeicao.addComida("Agua", 2.0, bebidas);
          
-        refeicao.mostrarPrincipais();
-        refeicao.mostrarSaladas();
-        refeicao.mostrarSobremesas();
-        refeicao.mostrarAdicionais();
-        refeicao.mostrarBebidas();
-
-
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);//seção critica é a parte do codigo que pode ser acessada opr 2 processos ao mesmo tempo
         Marmita funcaoCliente = new Marmita();
         funcaoCliente.cadastro();
         System.out.println("Selecione a opção:\n1 - desejo criar minha marmita\t2 - desejo o plano marmitas semanais");
         Double plano = scan.nextDouble();
         if (plano == 1){
-            funcaoCliente.inserirItens();
+            funcaoCliente.inserirItens(refeicao);
         }
 
  
     }
     public void cadastro(){
+        Scanner scan = new Scanner(System.in);//seção critica é a parte do codigo que pode ser acessada opr 2 processos ao mesmo tempo
         Cliente cliente = new Cliente();
         System.out.println("Bem vindo ao Sistema Marmitex PF\nPreencha os dados a seguir para completar o cadastro: ");
         System.out.println("Qual o seu nome: ");
@@ -82,25 +86,37 @@ class Marmita {
         cliente.adicionarCliente(cliente);
         System.out.println("Cadastro concluido com sucesso");
     }
-    public void inserirItens() {
-        ArrayList<Object> listaPedidos = new ArrayList<>();
+    public void inserirItens(Comida refeicao) {
+        Scanner scan = new Scanner(System.in);//seção critica é a parte do codigo que pode ser acessada opr 2 processos ao mesmo tempo
+        System.out.println(refeicao.listaBebidas);
+        System.out.println(refeicao.listaSaladas);
+
+
+        ArrayList<Componentes> listaPedidos = new ArrayList<Componentes>();
+        ArrayList<Componentes> listaTemp = new ArrayList<Componentes>();
+
+        int qualLista = 1;
         while (true){
-            System.out.println("Escolha um dos itens:\n");
-            refeicao.mostrarSaladas();
+            listaTemp = ((ArrayList<Componentes>) refeicao.listaTodasComidas.get(qualLista));
+            refeicao.toString(listaTemp, qualLista);
             int opcao = scan.nextInt(); 
-            for ( int i =0; i < refeicao.listaSaladas.size();i++){
-                if (opcao == i)   {
-                    Object variavel = refeicao.listaSaladas.get(i);
+            for ( int i = 0; i < listaTemp.size(); i++) {
+                if (opcao == i){
+                    Componentes variavel = listaTemp.get(i);
                     listaPedidos.add(variavel);
                     break;
                 }
             }
+
+            System.out.println(listaPedidos);
+
             System.out.println("Deseja escolher mais algum item:\n1: Sim\t2 - Nâo");
             Double maisItens = scan.nextDouble();
             if (maisItens == 1){
                 continue;
             }else{
-                break;
+                qualLista++;
+                
             }
         } 
         
